6edac46c990a875f550c2a94f495214232c6d127
Yanisse <yanisse321@gmail.com>
2022-11-03 14:39:56 +0100

    update

--- poo-restau/src/Model/BasketModel.php
+++ poo-restau/src/Model/BasketModel.php
@@ -12,7 +12,5 @@ class BasketModel extends DefaultModel{
     public function getBasket(){
         $statement = "SELECT * FROM ".$this->table." WHERE id_user = 1";
         return $this->getData($statement);
-        // $query = $this->pdo->query($statement, \PDO::FETCH_OBJ);
-        // $items =  $query->fetchAll();
     }
 }
