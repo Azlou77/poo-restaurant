bfb2c281ace85d067c9227ac189df9dafe0b841f
Ethan <92116172+SoraNoTami@users.noreply.github.com>
2022-11-04 16:31:38 +0100

    Merge some branch

--- poo-restau/templates/Menu/index.php
+++ poo-restau/templates/Menu/index.php
@@ -1,5 +1,5 @@
 
-        <h1 class="text-center">Les menus</h1>
+        <!-- <h1 class="text-center">Les menus</h1>
         <table class="table table-striped table-hover">
             <thead>
             <tr>
@@ -9,12 +9,59 @@
             </tr>
             </thead>
             <tbody>
-                <?php foreach($menus as $menu): ?>
-                <tr>
-                    <td><?= $menu->getId() ?></td>
-                    <td><?= $menu->getName() ?></td>
-                    <td><?= $menu->getPrice() ?></td>
-                </tr>
-                <?php endforeach; ?>    
             </tbody>
-        </table>
+        </table> -->
+
+        <div><img src="assets/img/home/banner%20promo.webp" width="1730" height="713" style="padding-bottom: 0px;margin-bottom: -102px;"><button class="btn btn-primary" type="button" style="background: rgb(247, 168, 0);border-style: none;font-size: 31px;width: 250px;padding-top: 0px;margin-left: 655px;padding-bottom: 0px;margin-top: -161px;margin-bottom: -5px;padding-left: 0px;padding-right: 0px;">Découvrez-les</button></div>
+    <div class="card">
+        <div class="container py-4 py-xl-5">
+            <div class="row mb-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <h2 class="fw-bold">Meilleurs menus</h2>
+                    <p class="w-lg-50"></p>
+                </div>
+            </div>
+            <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3" style="margin-left: 85px;">
+                <div class="col">
+                    <div style="width: 300px;height: 300px;"><img class="rounded img-fluid d-block w-100 fit-cover" style="height: 200px;" src="assets/img/meilleurs%20menus/master%20montagnard.jpg">
+                        <div class="py-4">
+                            <h4 class="text-center">Master Montagnard</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div style="width: 300px;height: 300px;"><img class="rounded img-fluid d-block w-100 fit-cover" style="height: 200px;" src="assets/img/meilleurs%20menus/mwii%20veggie%20whopper.jpg">
+                        <div class="py-4">
+                            <h4 class="text-center">MWII Cheese</h4>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div style="width: 300px;height: 300px;"><img class="rounded img-fluid d-block w-100 fit-cover" style="height: 200px;" src="assets/img/meilleurs%20menus/mwii%20whopper%20cheese.jpg">
+                        <div class="py-4">
+                            <h4 class="text-center">MWII Veggie Whooper</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container py-4 py-xl-5" style="margin-left: 85px;">
+            <div class="row mb-5">
+                <div class="col-md-8 col-xl-6 text-center mx-auto">
+                    <h2 class="fw-bold">Menus Bon Plans</h2>
+                    <p class="w-lg-50"></p>
+                </div>
+            </div>
+            <div class="row gy-4 row-cols-1 row-cols-md-2 row-cols-xl-3" style="margin-left: 85px;">
+            <?php foreach($menus as $menu): ?>
+                <div class="col">
+                    <div style="width: 300px;height: 300px;"><img class="rounded img-fluid d-block w-100 fit-cover" style="height: 200px;" src="assets/img/menus%20bon%20plans/<?= $menu->getImage() ?>.jpg">
+                        <div class="py-4">
+                            <h4 class="text-center" style="width: 329px;"><?= $menu->getName() ?></h4>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?> 
+            </div>
+        </div>
+    </div>
--- poo-restau/templates/base.php
+++ poo-restau/templates/base.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Burger King</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Navbar-Centered-Brand-Dark-icons.css">
+    <link rel="stylesheet" href="assets/css/Projects-Grid-images.css">
 </head>
 <body>
     <?= $content ?>
+    <?php require(ROOT.' ./View/include/footer.php') ?>
 </body>
 </html>
